2587ec8be7690db953e5c7852cd1065db3f4c5d0
Yzai Alarcon Franca <alarconfrancay@gmail.com>
2021-10-29 00:04:42 -0500

    Proyecto vasico de java, caulo de suma y resta

---
+++ src/calcular/Calcular.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calcular;
+
+/**
+ *
+ * @author alarc
+ */
+public class Calcular {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int salario = 5000;
+        int salarios = salario * 2;
+    
+        int a =100;
+        int b =7;
+        
+        int resultado= a/b;
+        int resultado1=a*b;
+        int resultado2=a+b;
+        int resultado3=a-b;
+        // TODO code application logic here
+        System.out.print("resultado de division= " + resultado);
+        System.out.print("resultado de multiplicacion= " + resultado1);
+        System.out.print("resultado de suma= " + resultado2);
+        System.out.print("resultado de resta= " + resultado3);
+    }
+    
+}
